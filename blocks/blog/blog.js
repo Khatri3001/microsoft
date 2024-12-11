bcbba8789492ac1542e0bb4dedfc4ff94c43c4a3
Abhay Singh Kushwah <abhay.kushwah@infogain.com>
2024-12-11 13:53:22 +0530

    fixes

--- blocks/blog/blog.js
+++ blocks/blog/blog.js
@@ -3,11 +3,17 @@ export default function decorateBlogs(container) {
     console.error("Blog container not found!");
     return;
   }
+
+  const heading = document.createElement('h2');
+  heading.textContent = "Related Blogs";
+  heading.classList.add('blog-heading');
+  container.parentElement.insertBefore(heading, container);
+
   const blogs = [...container.children];
 
   blogs.forEach((blog) => {
-    const meta = blog.querySelector('p'); 
-    const title = blog.querySelector('h3'); 
+    const meta = blog.querySelector('p');
+    const title = blog.querySelector('h3');
 
     if (meta && title) {
       blog.classList.add('styled-blog');
